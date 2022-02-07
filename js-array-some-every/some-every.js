87dca786421cb4ff4677e05846af4cfee5647149
HAO <39643014+Haody1064@users.noreply.github.com>
2022-02-06 18:24:49 -0800

    Update some-every.js

--- js-array-some-every/some-every.js
+++ js-array-some-every/some-every.js
@@ -22,7 +22,7 @@ Examples:
 
 function hasAZero(num){
     return num.toString().split('').some(function(val){
-        return val === 0;
+        return val === '0';
     })
 }
 
@@ -77,8 +77,9 @@ function hasCertainKey(arr, key) {
 }
 
 /*
-Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains
- that value for the specific key. Otherwise it should return false.
+Write a function called hasCertainValue which accepts an array of objects and a key, and a value, 
+    and returns true if every single object in the array contains
+    that value for the specific key. Otherwise it should return false.
 
 Examples:
     var arr = [
@@ -94,6 +95,8 @@ Examples:
 */
 
 function hasCertainValue(arr, key, searchValue) {
-    
+    return arr.every(function(val){
+        return val[key] === searchValue;
+    });
 
 }
