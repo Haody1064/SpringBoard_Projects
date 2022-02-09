ea1deb90c74d294df03efb24a1114b818953e210
HAO <39643014+Haody1064@users.noreply.github.com>
2022-02-09 12:35:10 -0800

    Update Destructuring-Exercise.js

--- Destructuring-Exercise.js
+++ Destructuring-Exercise.js
@@ -4,8 +4,10 @@
 let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
 let {numPlanets, yearNeptuneDiscovered} = facts;
 
-// console.log(numPlanets); // ?
-// console.log(yearNeptuneDiscovered); // ?
+ console.log(numPlanets); // ? 8
+console.log(yearNeptuneDiscovered); // ? 1846
+
+
 // Object Destructuring 2
 // What does the following code return/print?
 
@@ -17,25 +19,27 @@ let planetFacts = {
 
 let {numPlanets, ...discoveryYears} = planetFacts;
 
-// console.log(discoveryYears); // ?
+console.log(discoveryYears); // ? {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
 // Object Destructuring 3
 // What does the following code return/print?
-
 function getUserData({firstName, favoriteColor="green"}){
   return `Your name is ${firstName} and you like ${favoriteColor}`;
 }
 
-getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // ?
-getUserData({firstName: "Melissa"}) // ?
-getUserData({}) // ?
+getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // ? Your name is Alejandro and you like purple
+getUserData({firstName: "Melissa"}) // ? Your name is Melissa and you like green
+getUserData({}) // ? Your name is undefined and you like green
+
+
 // Array Destructuring 1
 // What does the following code return/print?
 
 let [first, second, third] = ["Maya", "Marisa", "Chi"];
 
-// console.log(first); // ?
-// console.log(second); // ?
-// console.log(third); // ?
+console.log(first); // ?
+console.log(second); // ?
+console.log(third); // ?
 
 
 
