d468f1bc59df3e7e8a8d304acf727e74e1e16b9f
HAO <39643014+Haody1064@users.noreply.github.com>
2022-02-22 08:09:57 -0800

    done

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,36 @@
 console.log("Let's get this party started!");
+const $gifArea = $('#giphyDisplay');
+const $searchInput = $('#search');
+
+function addGif(res) {
+    let numResults = res.data.length;
+    if (numResults) {
+      let randomIdx = Math.floor(Math.random() * numResults);
+      let $newCol = $("<div>", { class: "gifs" });
+      let $newGif = $("<img>", {
+        src: res.data[randomIdx].images.original.url,
+        class: "w-100"
+      });
+      $newCol.append($newGif);
+      $gifArea.append($newCol);
+    }
+  }
+
+$("form").on('submit', async function(evt){
+    evt.preventDefault();
+
+    let searchTerm = $searchInput.val();
+    $searchInput.val("");
+    
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", 
+    {params: {
+        q:searchTerm,
+        api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+    });
+    addGif(response.data);
+});
+
+$('#rmbtn').on('click', function(){
+    $gifArea.empty();
+})
