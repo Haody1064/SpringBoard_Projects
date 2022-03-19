4c87b2b23863241d2c2784be8d7568df8ff5ad2b
HAO <39643014+Haody1064@users.noreply.github.com>
2022-03-19 16:24:02 -0700

    need help

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -2,7 +2,7 @@
 
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -18,5 +18,29 @@ class SerialGenerator:
 
     >>> serial.generate()
     100
-    """
 
+    >>> serial.__repr__()
+    'Serialgenerator start=100 next=101'
+
+    """
+    def __init__(self, start = 0):
+        """Make a new generator, starting at start."""
+
+        self.start = self.next = start
+
+    def __repr__(self):
+        """Show representation."""
+
+        return f"Serialgenerator start={self.start} next={self.next}"
+
+    def generate(self):
+        """return next serial."""
+
+        self.next += 1
+        return self.next -1
+
+    def reset(self):
+        """Reset number to original start."""
+
+        self.next = self.start
+        
